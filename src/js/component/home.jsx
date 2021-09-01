f438b4ee977c2627fd18190f0ae8bf50fb8ba284
Diegogl91 <garciadiego03@gmail.com>
2021-09-01 22:00:28 +0000

    ejercicio spotify

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const useMultiAudio = urls => {
+	const [sources] = useState(
+		urls.map(url => {
+			return {
+				url,
+				audio: new Audio(url)
+			};
+		})
+	);
+
+	const [players, setPlayers] = useState(
+		urls.map(url => {
+			return {
+				url,
+				playing: false
+			};
+		})
+	);
+
+	const toggle = targetIndex => () => {
+		const newPlayers = [...players];
+		const currentIndex = players.findIndex(p => p.playing === true);
+		if (currentIndex !== -1 && currentIndex !== targetIndex) {
+			newPlayers[currentIndex].playing = false;
+			newPlayers[targetIndex].playing = true;
+		} else if (currentIndex !== -1) {
+			newPlayers[targetIndex].playing = false;
+		} else {
+			newPlayers[targetIndex].playing = true;
+		}
+		setPlayers(newPlayers);
+	};
+
+	useEffect(() => {
+		sources.forEach((source, i) => {
+			players[i].playing ? source.audio.play() : source.audio.pause();
+		});
+	}, [sources, players]);
+
+	useEffect(() => {
+		sources.forEach((source, i) => {
+			source.audio.addEventListener("ended", () => {
+				const newPlayers = [...players];
+				newPlayers[i].playing = false;
+				setPlayers(newPlayers);
+			});
+		});
+		return () => {
+			sources.forEach((source, i) => {
+				source.audio.removeEventListener("ended", () => {
+					const newPlayers = [...players];
+					newPlayers[i].playing = false;
+					setPlayers(newPlayers);
+				});
+			});
+		};
+	}, []);
+
+	return [players, toggle];
+};
+
+const MultiPlayer = ({ urls }) => {
+	const [players, toggle] = useMultiAudio(urls);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			{players.map((player, i) => (
+				<Player key={i} player={player} toggle={toggle(i)} />
+			))}
 		</div>
 	);
 };
 
-export default Home;
+const Player = ({ player, toggle }) => (
+	<div>
+		<p>Stream URL: {player.url}</p>
+		<button onClick={toggle}>{player.playing ? "Pause" : "Play"}</button>
+	</div>
+);
+export default MultiPlayer;
